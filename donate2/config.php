40a46a085ece35fca9a979c01f2c97b8c0db5443
Hyper-Coder <bryan.pikaardjr@outlook.com>
2016-07-13 14:02:49 -0400

    Update config.php

--- donate2/config.php
+++ donate2/config.php
@@ -1,27 +1,13 @@
 <?php
-	//start session in all pages
-	if (session_status() == PHP_SESSION_NONE)
-	{
+	if (session_status() == PHP_SESSION_NONE) {
 		session_start();
 	}
-	//PHP >= 5.4.0
-	//if(session_id() == '') { session_start(); } //uncomment this line if PHP < 5.4.0 and comment out line above
 
-	// sandbox or live
-	define('PPL_MODE', 'sandbox'); //change this to 'live' if you want this to work
+	define('PPL_MODE', 'live');
 
-	if(PPL_MODE=='sandbox')
-	{
-		define('PPL_API_USER', 'somepaypal_api.yahoo.co.uk');
-		define('PPL_API_PASSWORD', '123456789');
-		define('PPL_API_SIGNATURE', 'opupouopupo987kkkhkixlksjewNyJ2pEq.Gufar');
-	}
-	else
-	{
-		define('PPL_API_USER', 'somepaypal_api.yahoo.co.uk');
-		define('PPL_API_PASSWORD', '123456789');
-		define('PPL_API_SIGNATURE', 'opupouopupo987kkkhkixlksjewNyJ2pEq.Gufar');
-	}
+	define('PPL_API_USER', '');
+	define('PPL_API_PASSWORD', '');
+	define('PPL_API_SIGNATURE', '');
 
 	define('PPL_LANG', 'EN');
 
@@ -30,5 +16,5 @@
 	define('PPL_RETURN_URL', 'https://typicalbot.ml/donate/');
 	define('PPL_CANCEL_URL', 'https://typicalbot.ml/donate/');
 
-	define('PPL_CURRENCY_CODE', 'EUR');
+	define('PPL_CURRENCY_CODE', 'USD');
 ?>
